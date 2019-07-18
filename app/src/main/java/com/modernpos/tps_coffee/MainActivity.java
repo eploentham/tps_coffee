55a1e6a17d310484352d38e87934d708f9292586
Ekapop <eploentham@gmail.com>
2019-07-18 07:35:18 +0700

    1
    1

--- app/src/main/java/com/modernpos/tps_coffee/MainActivity.java
+++ app/src/main/java/com/modernpos/tps_coffee/MainActivity.java
@@ -1,13 +1,30 @@
 package com.modernpos.tps_coffee;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mainBtnMemb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        mainBtnMemb = findViewById(R.id.mainBtnMemb);
+        mainBtnMemb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MemberView.class));
+            }
+        });
     }
 }
