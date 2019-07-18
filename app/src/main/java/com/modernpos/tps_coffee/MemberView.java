56ebf773c305932b20a2de99ea2d37c9e99bdfbb
Ekapop <eploentham@gmail.com>
2019-07-18 17:11:23 +0700

    1
    1

--- app/src/main/java/com/modernpos/tps_coffee/MemberView.java
+++ app/src/main/java/com/modernpos/tps_coffee/MemberView.java
@@ -6,6 +6,7 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class MemberView  extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
